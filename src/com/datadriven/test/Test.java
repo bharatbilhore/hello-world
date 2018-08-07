96a977f24afce57f14427582459cd7d92df403b4
bharatbilhore <bilhore3@gmail.com>
2018-08-07 16:00:02 +0530

    Change in test class

--- src/com/datadriven/test/Test.java
+++ src/com/datadriven/test/Test.java
@@ -6,8 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("hiii");
 		System.out.println("hiii");
-		System.out.println("hiii");
-		System.out.println("hiii");
+		
 		
 	}
 
